b74729e9de876f9dee8ececf45fe35957a8ff6f5
Mateusz Choma <kontakt@coderoad.pl>
2021-05-31 07:27:31 +0200

    `forceUpdate`

--- src/Clock.js
+++ src/Clock.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 export class Clock extends React.Component {
+  constructor () {
+    super()
+
+    setInterval(() => {
+      this.forceUpdate()
+    }, 1000)
+  }
+
   render () {
     return <h1>{(new Date()).toLocaleString()}</h1>
   }
